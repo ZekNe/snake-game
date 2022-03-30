a1eab74a439c3a13e8320c8ded674d01135152b3
unknown <nzekic@hotmail.com>
2022-03-30 17:07:37 +0200

    created start button and snake

---
+++ index.js
@@ -0,0 +1,28 @@
+const canvas = document.getElementById('snakeCanvas');
+const ctx = canvas.getContext('2d');
+const Container = document.querySelector('.snakeContainer');
+const sbutton = Container.querySelector('.startButton');
+
+let snake = [{x: 150, y: 150 },
+{x: 140, y: 150},
+{x: 130, y: 150},
+{x: 120, y: 150},
+{x: 110, y: 150}];
+
+function drawSnakePart(snakePart) {
+    ctx.fillStyle = 'green'; ctx.strokestyle = 'darkgreen';
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() { 
+    snake.forEach(drawSnakePart); 
+}
+
+Container.addEventListener("click", (e) => {
+    if (e.target.matches("button")){
+        drawSnake()
+        sbutton.style.display="none";
+    }
+})
+
