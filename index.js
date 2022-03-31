240f6cbf98416e2d5a98e75a35792648220dcdb7
unknown <nzekic@hotmail.com>
2022-03-31 19:43:48 +0200

    horizontal movement added

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const ctx = canvas.getContext('2d');
 const Container = document.querySelector('.snakeContainer');
 const sbutton = Container.querySelector('.startButton');
 
-let snake = [{x: 150, y: 150 },
-{x: 140, y: 150},
-{x: 130, y: 150},
-{x: 120, y: 150},
-{x: 110, y: 150}];
+let snake = [{ x: 150, y: 150 },
+{ x: 140, y: 150 },
+{ x: 130, y: 150 },
+{ x: 120, y: 150 },
+{ x: 110, y: 150 }];
+
+let nextsnake = [{ x: 160, y: 150 },
+{ x: 150, y: 150 },
+{ x: 140, y: 150 },
+{ x: 130, y: 150 },
+{ x: 120, y: 150 }];
 
 function drawSnakePart(snakePart) {
     ctx.fillStyle = 'green'; ctx.strokestyle = 'darkgreen';
@@ -15,14 +21,20 @@ function drawSnakePart(snakePart) {
     ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
 
-function drawSnake() { 
-    snake.forEach(drawSnakePart); 
+function drawSnake() {
+    snake.forEach(drawSnakePart);
 }
 
 Container.addEventListener("click", (e) => {
-    if (e.target.matches("button")){
+    if (e.target.matches("button")) {
         drawSnake()
-        sbutton.style.display="none";
+        sbutton.style.display = "none";
     }
 })
+
+function advanceSnake(){
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    snake.unshift(head);
+    snake.pop();
+}
 
